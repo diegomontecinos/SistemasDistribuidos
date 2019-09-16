5d2c2bb1f9947b220af3adb1bc8f650b62bfe596
diegomontecinos <diegomontecinos@users.noreply.github.com>
2019-09-16 18:19:50 -0300

    Dcokerfile

--- Tarea1/Cliente.py
+++ Tarea1/Cliente.py
@@ -1,1 +1,12 @@
 import socket
+import time
+
+HOST = '127.0.0.1' #localhost
+PORT = 7000
+
+with socket.socket( socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+    client_socket.connect((HOST,PORT))
+    client_socket.sendall(bytearray("Mensaje del cliente", 'utf-8'))
+    in_message = client_socket.recv(1024)
+
+    print('respuesta servidor: ', in_message)
--- Tarea1/Servidor.py
+++ Tarea1/Servidor.py
@@ -1,5 +1,36 @@
 import socket
+import time
 
 HOST = '127.0.0.1' #localhost
-PORT = 5000 #listen port
+PORT = 7000 #listen port
+
+Waiting = True
+Connect = False
+
+with socket.socket( socket.AF_INET, socket.SOCK_STREAM) as host_socket:
+
+    host_socket.bind((HOST,PORT))
+    host_socket.listen(1)
+
+    print("Esperando conexion...\n")
+    
+    while (Waiting):
+        conn , addr = host_socket.accept()
+        print("Conexión establecida con", addr)
+        Connect = True
+        while(Connect):
+            Waiting = False
+            in_message = conn.recv(1024)
+            time.sleep(0.5)
+            if not in_message:
+                Connect = False
+                break   
+            else:
+                #conn.sendall(bytearray(in_message, 'utf-8'))
+                print(in_message)
+        
+            
+
+
+
 
